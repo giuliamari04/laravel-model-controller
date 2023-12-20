cf5ef406be78dc8dad153e615a39cce82989be4c
giuliamari04 <giuliamari04@gmail.com>
2023-12-20 15:01:33 +0100

    punto 7 completato

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -10,16 +10,28 @@
     @endforeach
 </ul> --}}
 
+<section class="container">
 <h1>Movies</h1>
-<ul>
+<div class="row py-3">
     @foreach ($movies as $movie)
-        <li>
-            {{-- <a href="{{ route('books.show', $movie->id) }}"> --}}
-                {{$movie->title }}
-            </a>
-        </li>
-    @endforeach
-</ul>
+<div class="col-3 py-3">
+     <div class="card h-100">
+            <img src="{{$movie->image}}" class="card-img-top" alt=" {{$movie->title }}">
+            <div class="card-body">
+              <h5 class="card-title"> {{$movie->title }}</h5>
+              <p class="card-text">
+                {{-- <h5>Original title: {{$movie->original_title }}</h5>
+                <h5>Nationality:  {{$movie->nationality }}</h5>
+                <h5>Released date:  {{$movie->date }}</h5> --}}
+                <h5>Vote:  {{$movie->vote }}</h5>
+              </p>
+              <a href="#" class="btn btn-primary">info movie</a>
+            </div>
+          </div>
+</div>
 
+    @endforeach
+</div>
+</section>
 
 @endsection
